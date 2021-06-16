0d0b0c33f5b99f80b1d775e0c1e0b0d3a54af334
lechiffre4 <toxcoa@gmail.com>
2021-06-16 11:03:13 +0200

    WebSite Begining

---
+++ database/db.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$serverName = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'fakesteamdb';
+
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+if (!$conn) {
+    die('Connection failed : ' . mysqli_connect_error());
+}
