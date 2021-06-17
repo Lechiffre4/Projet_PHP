7f2d9abdf82eb1d1f37f056739574ae1364e3695
lechiffre4 <toxcoa@gmail.com>
2021-06-17 14:47:05 +0200

    checkup invalide email

--- FakeStim/public/ValidationInscription.php
+++ FakeStim/public/ValidationInscription.php
@@ -13,15 +13,22 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage()); //Recup des erreurs
 }
 
-$reqRec = $bdd->prepare('INSERT INTO profiles(username, password) VALUES (:Username, :password)'); //Enregistrer un utilisateur dans la BDD
+$reqRec = $bdd->prepare('INSERT INTO profiles(username,email,password) VALUES (:Username, :email ,:password)'); //Enregistrer un utilisateur dans la BDD
 $reqPseudo = $bdd->prepare('SELECT * FROM profiles WHERE username = :Username'); //Vérification du pseudo
+$reqEmail= $bdd->prepare('SELECT * FROM profiles WHERE email=:email');
+
 $reqPseudo->execute(array(':Username' => htmlspecialchars($_POST['pseudo'])));
+$reqEmail->execute(array(':email' => htmlspecialchars($_POST['email'])));
 
 if ($reqPseudo->rowCount() > 0) { //Vérification de l'existence du pseudo
-    echo "Pseudo utilisé";
-} else {
+    echo "Pseudo ou Email déjà utilisé";
+    $reqRec=$bdd->prepare('SELECT * FROM profiles');
+} 
+
+else {
     $reqRec->execute(array(
             ':Username' => htmlspecialchars($_POST['pseudo']),
+            ':email'=> htmlspecialchars($_POST['email']),
             ':password' => password_hash($_POST['password'],PASSWORD_BCRYPT)
             )); //Envois du pseudo et mdp à la BDD
 
